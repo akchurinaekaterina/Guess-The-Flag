785f5b929f4fcbec77aba494d2dbefaf264f14da
Ekaterina Akchurina <akchurina.ekaterina@gmail.com>
2020-09-17 17:41:54 +0300

    Initial Commit

---
+++ Guess The Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess The Flag
+//
+//  Created by Ekaterina Akchurina on 17.09.2020.
+//  Copyright © 2020 Ekaterina Akchurina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
