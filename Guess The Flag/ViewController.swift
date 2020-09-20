a466f814358cdd25f071d307fac994caa2af5c81
Ekaterina Akchurina <akchurina.ekaterina@gmail.com>
2020-09-20 22:40:23 +0300

    updated

--- Guess The Flag/ViewController.swift
+++ Guess The Flag/ViewController.swift
@@ -9,10 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if sender.tag == correctAnswer {
+            score += 1
+            showAlert(title: "Correct!", messag: "Your score is \(score)")
+        } else {
+            score -= 1
+            showAlert(title: "Ooops!", messag: "This is \(countries[correctAnswer].uppercased()). Now your score is \(score)")
+        }
+        print(score)
+    }
+    
+    
+    var countries = [String]()
+    var buttons = [UIButton]()
+    var score = 0
+    var correctAnswer = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        buttons += [button1, button2, button3]
+        
+        for button in buttons {
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor.lightGray.cgColor
+        }
+        
+        
+        
+        askQuestions()
+        
+        
+
+    }
+    
+    func askQuestions(_: UIAlertAction! = nil) {
+        
+        countries.shuffle()
+        
+        for button in buttons {
+            guard let index = buttons.firstIndex(of: button) else {return}
+            button.setImage(UIImage(named: countries[index]), for: .normal)
+        }
+        
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
+        
+    }
+    
+    func showAlert (title: String, messag: String) {
+        let alert = UIAlertController(title: title, message: messag, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: askQuestions))
+        present(alert, animated: true)
+        
     }
 
 
